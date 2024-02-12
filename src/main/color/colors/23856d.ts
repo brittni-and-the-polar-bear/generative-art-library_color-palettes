57ce92acc443c4eb42c3ba3e1cc42d80de512740
azurepolarbear <azurepolarbear@users.noreply.github.com>
2024-02-12 00:23:50 -0600

    Add new color (23856d)

---
+++ src/main/color/colors/23856d.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2024 Brittni Watkins.
+ *
+ * This file is a part of brittni and the polar bear's Generative Art Library,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+// noinspection ES6PreferShortImport
+import {PaletteColor} from "../palette-color";
+
+/**
+ * @public
+ */
+const _23856d: PaletteColor = {
+    rgb: {r: 35, g: 133, b: 109},
+    hsl: {h: 165, s: 58, l: 33},
+    hexString: '#23856D',
+    name: 'accent green blue',
+    htmlName: 'seagreen',
+    wikipediaName: 'myrtle green'
+};
+
+export {_23856d};
