7aa67eb07e450a52cdf38223493956e3b85f0e97
azurepolarbear <azurepolarbear@users.noreply.github.com>
2024-03-10 18:36:19 -0500

    #1 Add new color for blue-lily color palette

---
+++ src/main/color/colors/fafeff.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2024 brittni and the polar bear LLC.
+ *
+ * This file is a part of brittni and the polar bear's Generative Art Library,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+import {PaletteColor} from '../palette-color';
+
+const _fafeff: PaletteColor = {
+    rgb: {r: 250, g: 254, b: 255},
+    hsl: {h: 192, s: 100, l: 99},
+    hexString: '#FAFEFF',
+    name: 'polar bear in a blizzard',
+    htmlName: 'white',
+    wikipediaName: 'white'
+}
+
+export {_fafeff};
